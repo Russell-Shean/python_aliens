cbaed2e8b2b36b5dc33aae62454a45db7594fffa
Russ <russshean@gmail.com>
2023-04-03 23:19:11 -0400

    make screen bigger

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ class Settings:
 	def __init__(self):
 		"""Initialize the game settings class"""
 
-		self.screen_width = 500
-		self.screen_height = 500
+		self.screen_width = 800
+		self.screen_height = 800
 		self.bg_color = (175, 238, 238)

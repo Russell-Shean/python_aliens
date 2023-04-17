547152cb5cc4123b1276c7914364ea821cbaa466
Russ <russshean@gmail.com>
2023-04-17 10:01:04 -0400

    create bullet and oranges classes

---
+++ bullet.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+	"""A class to manage all the bullets together as one group"""
+
+
+	def __init__(self, ai_game):
+		"""Create a bullet object at the ship's current position."""
+
+		# This causes properties to be inherited from sprite
+		super().__init__()
+
+		self.screen = ai_game.screen
+		self.settings = ai_game.settings
+		self.color = self.settings.bullet_color
+
+		# create a bullet rectangle at (0, 0) and then set its position
+		self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+		self.rect.midtop = ai_game.rect.midtop
+
+
+		# store the bullet's position as a decimal value
+		self.y = float(self.rect.y)
---
+++ oranges.py
@@ -0,0 +1,30 @@
+# oranges.py
+
+# recreate the bullet class using a picture of an orange
+
+
+import pygame
+from pygame.sprite import Sprite
+
+class Orange(Sprite):
+	"""A class to manage all the bullets together as one group"""
+
+
+	def __init__(self, ai_game):
+		"""Create a bullet object at the ship's current position."""
+
+		# This causes properties to be inherited from sprite
+		super().__init__()
+
+		self.screen = ai_game.screen
+		self.settings = ai_game.settings
+		#self.color = self.settings.bullet_color
+
+		# create an orange rectangle at (0, 0) and then set its position
+		self.image = pygame.image.load("images/orange.png")
+		self.rect = self.image.get_rect()
+		self.rect.midtop = ai_game.rect.midtop
+
+
+		# store the bullet's position as a decimal value
+		self.y = float(self.rect.y)
